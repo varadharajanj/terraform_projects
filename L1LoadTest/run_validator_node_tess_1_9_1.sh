b01c033d6fc34f2cff8a766eaaf8e89254eae43f
VJAlkimi <vj@alkimiexchange.com>
2023-03-21 00:14:48 +0000

    initial version

---
+++ L1LoadTest/run_validator_node_tess_1_9_1.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+cd /root/L1_node/
+export CL_KEYSTORE=key.p12 CL_KEYALIAS=walletalias CL_PASSWORD=welcome123
+rm -rf data/ logs/
+java "-Xms1024M" "-Xmx3G" "-Xss256K" -cp tess_1.9.1_cl-node.jar org.tessellation.Main run-validator --ip XX.XX.XX.XX --public-port 9065 --p2p-port 9066 --cli-port 9067 -e testnet --collateral 0 &
